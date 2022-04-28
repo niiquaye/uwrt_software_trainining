d4c87a1bede5b52a3dd2967cea54d0e70d0516b9
Andrew <andrew2002zhao@gmail.com>
2022-04-28 17:11:15 -0400

    init commit

---
+++ src/clear_turtles_node.cpp
@@ -0,0 +1,23 @@
+#include "clear_turtles_node.hpp";
+
+using namespace composition;
+
+clear_turtles_node::clear_turtles_node(const rclcpp::NodeOptions &options) {
+    client = create_client<turtlesim::srv::Kill>("/kill");
+
+    timer_cb = create_wall_timer(2s, std::bind(kill, this));
+}
+
+void clear_turtles_node::kill() {
+    //kill all the nodes by sending a request to each name
+    for(auto turtle : nodes) {
+        //send a kill request to turtlesim for each turtle name
+        //first check if the turtle exists
+        //then delete the turtle
+        //make requests i guess
+        sharedReq = std::make_shared<rclcpp::Client<turtlesim::srv::Kill>::SharedRequest> (turtle);
+        client.async_send_request(sharedReq);
+        
+    }
+
+}
