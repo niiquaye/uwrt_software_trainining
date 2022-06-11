fce41d82ad19d35c6a3bc19775ac749b71f2bf38
Andrew <andrew2002zhao@gmail.com>
2022-06-10 20:10:38 -0600

    fixed the timer for reset turtle

--- src/reset_turtle_node.cpp
+++ src/reset_turtle_node.cpp
@@ -6,15 +6,15 @@ reset_turtle_node::reset_turtle_node(const rclcpp::NodeOptions &options) {
 }
 void reset_moving_turtle() {
     //call turtlesim teleport absolute
-    timer_cb = create_timer(2s, std::bind(reset, this));
+    timer_cb = create_timer(std::chrono::duration<int, std::chrono::seconds::period>(1), std::bind(&reset, this));
 }
 
 void reset() {
     sharedPtr = std::make_shared<rclcpp::Client<rclcpp::srv::TeleportAbsolute>>::SharedPtr>(25, 25, 0, "moving_turtle"); // how do i interact with turtleX lol the documentation doesn't take a name as  input
-    auto successCallback = [] -> { 
+    auto successCallback = [this]() -> { 
         std::cout << "finished";
     }
-    auto failureCallback = [] -> {
+    auto failureCallback = [this]() -> {
         std::cout << "Failure";
     }
     client -> send_async_request(sharedPtr, successCallback, failureCallback);
