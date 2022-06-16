714e3645473c417d282f76588b081321439b9f0c
Andrew <andrew2002zhao@gmail.com>
2022-06-16 05:59:09 -0600

    added all the different turtle requests to cmakelists

--- include/software_training_assignment/spawn_turtles_node.hpp
+++ include/software_training_assignment/spawn_turtles_node.hpp
@@ -5,8 +5,6 @@
 class spawn_turtles : public rclcpp::Node {
 private:
     /* data */
-
-    
     rclcpp::Client<turtlesim::srv::Spawn>* client;
     rclcpp::Client<turtlesim::srv::Spawn>::sharedRequest* sharedRequest;
     rclcpp::timer::WallTimer timer_cb;
