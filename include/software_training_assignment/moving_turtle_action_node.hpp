9c95f4e3520abc99e7f82f32dcf86cc8043a7dd4
Andrew <andrew2002zhao@gmail.com>
2022-06-26 15:45:25 -0600

    updated action server header file

--- include/software_training_assignment/moving_turtle_action_node.hpp
+++ include/software_training_assignment/moving_turtle_action_node.hpp
@@ -1,7 +1,37 @@
 #include <cstdlib>
-#include <rclcpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+#include <rclcpp/rclcpp.hpp>
 
+#include <turtlesim/msg/pose.hpp>
+#include <software_training/action/software.hpp>
+
+namespace composition{
 class moving_turtle_action_node : public rclcpp::Node {
-    moving_turtle_action_node();
+    public:
+
+        moving_turtle_action_node();
+        using GoalHandleActionServer =
+            rclcpp_action::ServerGoalHandle<software_training::action::Software>;
+    private:
+
+        rclcpp_action::Server<software_training::action::Software>::SharedPtr action_server;
+
+        rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher;
+
+        rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subscriber;
+
+        //copied some code from software training, since action server is hard to figure out
+        SOFTWARE_TRAINING_LOCAL
+        rclcpp_action::GoalResponse handle_goal(
+            const rclcpp_action::GoalUUID &uuid,
+            std::shared_ptr<const software_training::action::Software::Goal> goal);
+
     
+        SOFTWARE_TRAINING_LOCAL
+        rclcpp_action::CancelResponse
+        handle_cancel(const std::shared_ptr<GoalHandleActionServer> goal_handle);
+
+        static constexpr unsigned int QUEUE{10};
+
+};
 }
